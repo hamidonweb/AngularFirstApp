71cb51e371b2c8cbae94b81eb122a0f416ccd14e
h.anjum <hamidanjum7@gmail.com>
2023-05-29 00:23:52 +0300

    commited from main branch

--- src/app/Mycomponents/todos/todos.component.ts
+++ src/app/Mycomponents/todos/todos.component.ts
@@ -16,16 +16,16 @@ Deletetodo: any;
 constructor(){
   this.todos = [
 {sno:1,
-  title:"this is title play after 106 minutes",
-  desc:"Description",
+  title:"this is title play after 106 minutes ",
+  desc:"Description", 
   active: true},
   {sno:2,
-    title:"this is another title play after 106 minutes",
+    title:"this is another title play after 106 minutes ",
     desc:"Description 2",
     active: false},
     {sno:3,
       title:"this is another title 2 play after 106 minutes",
-      desc:"Description 3",
+      desc:"Description 3" ,
       active: true}
 ]
 
